ba1333b1080cb897a1d968c6323f78f8cca56e02
isFY <fy77645@126.com>
2025-01-05 04:09:53 +0800

    docs

--- src/index.js
+++ src/index.js
@@ -5,24 +5,36 @@ const app = new Hono()
 
 
 app.get('/:user/:repo',async c=>{
-	const user = c.req.param('user')
-	const repo = c.req.param('repo')
-	const targetUrl = `https://${user}.github.io/${repo}`
-	return handleProxy(targetUrl,c)
+	return getHome(c)
 })
 
+app.get('/:user/:repo/:docs',async c=>{
+	const docs = c.req.param('docs')
+	if(docs === 'docs') return getHome(c,true)
+	else return getAssets(c)
+})
 
 app.get('/:repo/*',async c=>{
+	return getAssets(c)
+})
+
+export default app
+
+function getHome(c,isDocs){
+	const user = c.req.param('user')
+	const repo = c.req.param('repo')
+	const targetUrl = `https://${user}.github.io/${repo}${isDocs?'/docs':''}`
+	return handleProxy(targetUrl,c)
+}
+
+function getAssets(c) {
 	const path = c.req.path
 	const referer = c.req.header('referer')
 	const url = new URL(referer)
 	const user = url.pathname.split('/')[1]
 	const targetUrl = `https://${user}.github.io${path}`
 	return handleProxy(targetUrl,c)
-})
-
-export default app
-
+}
 
 async function handleProxy(url,c){
 	const init = {
